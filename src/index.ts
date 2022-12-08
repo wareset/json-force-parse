34819e0e8e461cff6d474bcedce5c0b049c67932
rease <cftcview@gmail.com>
2022-12-08 10:41:38 +0500

    optimize

--- src/index.ts
+++ src/index.ts
@@ -1,66 +1,69 @@
+const REG_IS_NOT_EMPTY = /^\S$/
+const REG_IS_NOT_NEW_LINE = /^[^\r\n\u2028\u2029]$/
+
 export default function json_with_comments_parse(source: string): any {
   const env: [string, any][] = []
   let cur: [string, any] = ['[', env]
 
-  let raw = ''
-  let c: string, s: string, b: boolean
+  let raw: string[] = []
+  let c: string, s: string, b: boolean, n: number
 
   let key: string
-  const save_any = (obj: any): any => {
+  const save_val = (obj: any): any => {
     cur[0] === '[' ? cur[1].push(obj) : cur[1][key] = obj
     return obj
   }
   const save_raw = (): void => {
-    if (raw) {
-      if (raw[0] === '"') save_any(JSON.parse(raw))
-      else if (raw === 'true' || raw === 'false') save_any(raw === 'true')
-      else if (raw === 'null') save_any(null)
-      else save_any(+raw)
-      raw = ''
+    if (raw.length) {
+      s = raw.join('')
+      if (raw[0] === '"') save_val(JSON.parse(s))
+      else if ((b = s === 'true') || s === 'false') save_val(b)
+      else if (s === 'null') save_val(null)
+      else save_val(+s)
+      raw = []
     }
   }
 
   for (let l = source.length, i = 0; i < l; i++) {
-    if (/^\S$/.test(c = source[i])) {
+    if (REG_IS_NOT_EMPTY.test(c = source[i])) {
       switch (c) {
         case ',':
           save_raw()
           break
-        case ':':
-          key = raw[0] === '"' ? JSON.parse(raw) : raw, raw = ''
-          break
         case '[':
-          env.push(cur = [c, save_any([])])
+          env.push(cur = [c, save_val([])])
           break
         case '{':
-          env.push(cur = [c, save_any({})])
+          env.push(cur = [c, save_val({})])
           break
         case ']':
         case '}':
-          save_raw(), env.pop(), cur = env[env.length - 1]
+          save_raw()
+          env.pop(), cur = env[env.length - 1]
+          break
+        case ':':
+          s = raw.join('')
+          key = raw[0] === '"' ? JSON.parse(s) : s, raw = []
           break
         case '"':
-          raw = c
-          for (b = !1; ++i < l && (raw += source[i],
-          b || source[i] !== c); b = b ? !1 : source[i] === '\\');
+          raw = [c]
+          for (b = false; ++i < l && (raw.push(source[i]),
+          b || source[i] !== c); b = b ? false : source[i] === '\\');
           break
         case '/':
           if ((s = source[i + 1]) === '*') {
-            s = raw, raw = c
-            for (;++i < l && (raw += source[i],
-            !(raw.length > 3 && source[i] === '/' && source[i - 1] === '*')););
-            raw = s
-          } else if (s === '/') {
-            s = raw, raw = c
-            for (;++i < l && (raw += source[i],
-            /[^\r\n\u2028\u2029]/.test(source[i])););
-            raw = s
+            i++
+            for (n = 0; ++i < l &&
+            !(n++ && source[i] === c && source[i - 1] === s););
+          } else if (s === c) {
+            i++
+            for (;++i < l && REG_IS_NOT_NEW_LINE.test(source[i]););
           } else {
-            raw += c
+            raw.push(c)
           }
           break
         default:
-          raw += c
+          raw.push(c)
       }
     }
   }
