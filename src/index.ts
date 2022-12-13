b49cb8749d3a87926e537936a3da3dae93927c34
rease <cftcview@gmail.com>
2022-12-14 04:15:28 +0500

    optimize

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,15 @@
 const REG_IS_NOT_EMPTY = /^\S$/
 const REG_IS_NOT_NEW_LINE = /^[^\r\n\u2028\u2029]$/
 
+const META: any = { b: '\b', t: '\t', n: '\n', f: '\f', r: '\r' }
+
+// const get_escaped_char = (s: string, i: number, i2: number): string => {
+//   let res = s[i] || ''
+//   if (i === i2) res in META && (res = META[s[i]])
+//   else res = String.fromCharCode(parseInt(s[++i] + s[++i] + s[++i] + s[++i], 16))
+//   return res
+// }
+
 export default function json_with_comments_parse(source: string): any {
   const env: [string, any][] = []
   let cur: [string, any] = ['[', env]
@@ -16,7 +25,7 @@ export default function json_with_comments_parse(source: string): any {
   const save_raw = (): void => {
     if (raw.length) {
       s = raw.join('')
-      if (raw[0] === '"') save_val(JSON.parse(s))
+      if (raw[0] === '') save_val(s)
       else if ((b = s === 'true') || s === 'false') save_val(b)
       else if (s === 'null') save_val(null)
       else save_val(+s)
@@ -42,13 +51,29 @@ export default function json_with_comments_parse(source: string): any {
           env.pop(), cur = env[env.length - 1]
           break
         case ':':
-          s = raw.join('')
-          key = raw[0] === '"' ? JSON.parse(s) : s, raw = []
+          key = raw.join(''), raw = []
           break
         case '"':
-          raw = [c]
-          for (b = false; ++i < l && (raw.push(source[i]),
-          b || source[i] !== c); b = b ? false : source[i] === '\\');
+          raw = ['']
+          // for (b = false; ++i < l && (raw.push(source[i]),
+          // b || source[i] !== c); b = b ? false : source[i] === '\\');
+          // for (;++i < l && source[i] !== '"';) {
+          //   raw.push(source[i] !== '\\' ? source[i]
+          //     : get_escaped_char(source, ++i, source[i] !== 'u' ? i : i += 4))
+          // }
+          for (;++i < l && source[i] !== c;) {
+            raw.push(
+              source[i] !== '\\'
+                ? source[i]
+                : source[++i] in META
+                  ? META[source[i]]
+                  : source[i] === 'u'
+                    ? String.fromCharCode(parseInt(
+                      source[++i] + source[++i] + source[++i] + source[++i], 16
+                    ))
+                    : source[i] || ''
+            )
+          }
           break
         case '/':
           if ((s = source[i + 1]) === '*') {
